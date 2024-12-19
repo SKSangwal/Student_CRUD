b12317fb72c8b6ef3caf13c588de2e8941befcd2
SKSangwal <sandeepkumarsangwal21@gmail.com>
2024-12-19 14:18:01 +0530

    push

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::post('/create', [App\Http\Controllers\WebController::class, 'create'])->name('create');
 Route::get('/view/{id?}', [App\Http\Controllers\WebController::class, 'view'])->name('view');
